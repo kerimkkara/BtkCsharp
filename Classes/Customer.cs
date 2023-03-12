b59899ab55734aa9aaa78852be4595938e237edd
Kerim Kara <kara.kerim@outlook.com>
2023-03-12 11:28:34 +0300

    Classes Completed

---
+++ Classes/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Customer
+    {
+        //field
+
+        // public string FirstName;
+
+        //property
+
+        public int Id { get; set; }
+
+        //private string _firstName;
+        //public string Name {
+        //    get {
+        //        return "Mr. " + _firstName;
+        //    } 
+        //    set {
+        //        _firstName = value;
+        //    } 
+        //}
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.Id= 1;
+            customer.Name = "Test";
+
+            Customer customer2 = new Customer
+            { 
+                Name= "Test",
+                Id= 2,
+                City= "İstanbul",
+                Address = "test"
+
+            };
+
+            Console.WriteLine(customer2.Name);
+        }
+
+    }
+}
